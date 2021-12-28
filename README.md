fba30acba5134d45c0796594f20932aea1081096
fujian <fujian.zhu@tenclass.com>
2021-12-28 12:12:14 +0800

    修改README文件

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # YCU_Supervisory_System
 YCU管理系统（毕设）
+
+
+### 安装依赖
+- pip install -r requirements.txt
---
+++ YCU_Supervisory_System/__init__.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from YCU_Supervisory_System.routers import{
+
+}
+
+api_router = APIRouter()
+api_router.include_router()
